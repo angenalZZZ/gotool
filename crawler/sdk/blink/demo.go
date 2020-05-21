32acdabdf16a71f190aff953d1c498f2d25e6247
yangzhou <angenal@hotmail.com>
2020-05-21 13:38:38 +0800

    update

--- crawler/sdk/blink/demo.go
+++ crawler/sdk/blink/demo.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"github.com/raintean/blink"
+	"log"
+)
 
+// go build -tags="bdebug" -ldflags="-H windowsgui" -o ../table25.exe ./crawler/sdk/blink/demo.go
+func main() {
+	urlTarget := "https://www.baidu.com/"
+
+	//启用调试模式
+	blink.SetDebugMode(true)
+
+	//初始化blink
+	err := blink.InitBlink()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	view := blink.NewWebView(false, 1266, 720)
+	view.LoadURL(urlTarget)
+	view.MoveToCenter()
+	view.ShowWindow()
+	view.ShowDevTools()
+
+	<-make(chan struct{})
 }
