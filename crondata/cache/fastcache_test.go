45deff6b3481077879494ee62f478eb0d64132a7
yangzhou <angenal@hotmail.com>
2020-06-02 09:30:58 +0800

    update

--- crondata/cache/fastcache_test.go
+++ crondata/cache/fastcache_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"github.com/angenalZZZ/gofunc/data/random"
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 )
 
 func TestCacheDirName(t *testing.T) {
@@ -13,4 +15,15 @@ func TestCacheDirName(t *testing.T) {
 	basePath, _ = filepath.Abs(os.Args[0])
 	basePath = filepath.Dir(basePath)
 	t.Log(basePath)
+}
+
+func BenchmarkGetCacheWriter(b *testing.B) {
+	b.StopTimer()
+	CacheSaveToFile = 10 * time.Second
+	CacheBackgroundWorker()
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		w := GetCacheWriter()
+		_, _ = w.Write([]byte(random.AlphaNumberLower(20)))
+	}
 }
